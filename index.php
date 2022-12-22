455bf2c57906bcfef51a08063686d6708ed60a30
GLBDM <glebaot@gmail.com>
2022-12-22 16:50:30 +0100

    22/12/2022 fin de journée

--- index.php
+++ index.php
@@ -2,11 +2,42 @@
 //Template name: Index
 ?>
 
-<main class="background">
+<main>
+    <div class="background"></div>
+    <div class="margin">
+        <section id="recentNews">
 
-    
+            <?php $recentPosts = new WP_Query();
+            $recentPosts->query('showposts=3');?>
 
 
+            <?php while ($recentPosts->have_posts()) : $recentPosts->the_post(); ?>
+            <?php $totalcomments = get_comments_number(); ?>
+
+                <div class="card" style="background-image: url('<?php the_post_thumbnail_url() ?>')">
+                    <a href="<?php the_permalink() ?>">
+                        <?php the_title(); ?>
+                        <span><?php echo $totalcomments; ?><span>
+                    </a>
+                </div>
+
+            <?php endwhile; ?>
+
+        </section>
+        <div class="container">
+            <div class="left">
+                <section class="news">
+                    <div class="title">
+                        <h2>News</h2>
+                    </div>
+                    <div class="content">
+                        
+                    </div>
+                </section>
+            </div>
+            <div class="right"></div>
+        </div>        
+    </div>
 </main>
 
 
